24ba72c6388bb2f3015729c6c4546c291e67406f
atwzj233 <atwzj233@gmail.com>
2020-12-25 21:37:53 +0800

    ICLevelTab 在没有选择分包的情况下会提示

--- app/src/main/java/org/wvt/horizonmgr/ui/modulemanager/ICLevelTab.kt
+++ app/src/main/java/org/wvt/horizonmgr/ui/modulemanager/ICLevelTab.kt
@@ -1,12 +1,15 @@
 package org.wvt.horizonmgr.ui.modulemanager
 
+import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
@@ -17,56 +20,81 @@ internal fun ICLevelTab(
     vm: ICLevelTabViewModel
 ) {
     val items by vm.levels.collectAsState()
+    val state by vm.state.collectAsState()
 
     val scope = rememberCoroutineScope()
     var progressDialogState by remember { mutableStateOf<ProgressDialogState?>(null) }
     val inputDialogState = remember { InputDialogHostState() }
 
-    Box(modifier = Modifier.fillMaxSize()) {
-        if (items.isEmpty()) {
-            EmptyPage(Modifier.matchParentSize()) {
-                Text("当前还没有地图")
+
+    Crossfade(current = state) {
+        when (it) {
+            ICLevelTabViewModel.State.Loading -> {
+                Box(modifier = Modifier.fillMaxSize()) {
+                    CircularProgressIndicator(Modifier.align(Alignment.Center))
+                }
             }
-        } else LazyColumn(contentPadding = PaddingValues(bottom = 64.dp)) {
-            itemsIndexed(items = items) { index, item ->
-                LevelItem(
-                    modifier = Modifier.padding(16.dp),
-                    title = item.name,
-                    screenshot = item.screenshot,
-                    onRenameClicked = {
-                        scope.launch {
-                            val result: InputDialogHostState.DialogResult =
-                                inputDialogState.showDialog("请输入新名称", "新名称")
-                            if (result is InputDialogHostState.DialogResult.Confirm) {
-                                progressDialogState = ProgressDialogState.Loading("正在重命名")
-                                try {
-                                    vm.renameLevel(item.path, result.name)
-                                } catch (e: Exception) {
-                                    e.printStackTrace()
-                                    progressDialogState =
-                                        ProgressDialogState.Failed("重命名失败", e.localizedMessage)
-                                    return@launch
-                                }
-                                progressDialogState = ProgressDialogState.Finished("重命名成功")
-                                vm.load()
-                            }
-                        }
-                    },
-                    onDeleteClicked = {
-                        scope.launch {
-                            progressDialogState = ProgressDialogState.Loading("正在删除")
-                            try {
-                                vm.deleteLevel(item.path)
-                            } catch (e: Exception) {
-                                progressDialogState =
-                                    ProgressDialogState.Failed("删除失败", e.localizedMessage ?: "")
-                                return@launch
-                            }
-                            progressDialogState = ProgressDialogState.Finished("删除完成")
-                            vm.load()
-                        }
+            ICLevelTabViewModel.State.PackageNotSelected -> {
+                EmptyPage(Modifier.fillMaxSize()) {
+                    Text("你还没有选择分包")
+                }
+            }
+            ICLevelTabViewModel.State.OK -> {
+                if (items.isEmpty()) {
+                    EmptyPage(Modifier.fillMaxSize()) {
+                        Text("当前还没有地图")
                     }
-                )
+                } else LazyColumn(
+                    modifier = Modifier.fillMaxSize(),
+                    contentPadding = PaddingValues(bottom = 64.dp)
+                ) {
+                    itemsIndexed(items = items) { index, item ->
+                        LevelItem(
+                            modifier = Modifier.padding(16.dp),
+                            title = item.name,
+                            screenshot = item.screenshot,
+                            onRenameClicked = {
+                                scope.launch {
+                                    val result: InputDialogHostState.DialogResult =
+                                        inputDialogState.showDialog("请输入新名称", "新名称")
+                                    if (result is InputDialogHostState.DialogResult.Confirm) {
+                                        progressDialogState = ProgressDialogState.Loading("正在重命名")
+                                        try {
+                                            vm.renameLevel(item.path, result.name)
+                                        } catch (e: Exception) {
+                                            e.printStackTrace()
+                                            progressDialogState =
+                                                ProgressDialogState.Failed(
+                                                    "重命名失败",
+                                                    e.localizedMessage ?: ""
+                                                )
+                                            return@launch
+                                        }
+                                        progressDialogState = ProgressDialogState.Finished("重命名成功")
+                                        vm.load()
+                                    }
+                                }
+                            },
+                            onDeleteClicked = {
+                                scope.launch {
+                                    progressDialogState = ProgressDialogState.Loading("正在删除")
+                                    try {
+                                        vm.deleteLevel(item.path)
+                                    } catch (e: Exception) {
+                                        progressDialogState =
+                                            ProgressDialogState.Failed(
+                                                "删除失败",
+                                                e.localizedMessage ?: ""
+                                            )
+                                        return@launch
+                                    }
+                                    progressDialogState = ProgressDialogState.Finished("删除完成")
+                                    vm.load()
+                                }
+                            }
+                        )
+                    }
+                }
             }
         }
     }
--- app/src/main/java/org/wvt/horizonmgr/ui/modulemanager/ICLevelTabViewModel.kt
+++ app/src/main/java/org/wvt/horizonmgr/ui/modulemanager/ICLevelTabViewModel.kt
@@ -1,7 +1,9 @@
 package org.wvt.horizonmgr.ui.modulemanager
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import org.wvt.horizonmgr.DependenciesContainer
@@ -10,6 +12,14 @@ import org.wvt.horizonmgr.legacyservice.HorizonManager
 class ICLevelTabViewModel(
     dependenciesContainer: DependenciesContainer
 ) : ViewModel() {
+    sealed class State {
+        object Loading : State()
+        object PackageNotSelected : State()
+        object OK : State()
+    }
+
+    val state = MutableStateFlow<State>(State.Loading)
+
     private val hzmgr = dependenciesContainer.horizonManager
 
     val levels = MutableStateFlow<List<HorizonManager.LevelInfo>>(emptyList())
@@ -17,6 +27,10 @@ class ICLevelTabViewModel(
     private var packageUUID: String? = null
 
     fun setPackage(uuid: String?) {
+        Log.d("ICLevelTabVM", "new package: $uuid")
+        if (uuid == null) {
+            state.value = State.PackageNotSelected
+        }
         packageUUID = uuid
     }
 
@@ -25,14 +39,16 @@ class ICLevelTabViewModel(
     }
 
     fun load() {
-        viewModelScope.launch {
+        viewModelScope.launch(Dispatchers.IO) {
             packageUUID?.let {
+                state.value = State.Loading
                 try {
                     levels.value = hzmgr.getICLevels(it)
                 } catch (e: Exception) {
                     // TODO 显示错误信息
                     e.printStackTrace()
                 }
+                state.value = State.OK
             } ?: run {
                 levels.value = emptyList()
             }
