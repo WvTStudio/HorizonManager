8c990a5f53326d3d52e07cdaf7a65569d6aee93b
atwzj233 <atwzj233@gmail.com>
2021-01-10 20:27:02 +0800

    Add dependencies for unit test

--- webapi/build.gradle.kts
+++ webapi/build.gradle.kts
@@ -13,8 +13,7 @@ android {
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-
-    buildTypes {
+    /*buildTypes {
         create("alpha") {
             isMinifyEnabled = true
             proguardFiles = mutableListOf(
@@ -29,7 +28,7 @@ android {
                 file("proguard-rules.pro")
             )
         }
-    }
+    }*/
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -43,10 +42,13 @@ dependencies {
     implementation("androidx.core:core-ktx:1.3.2")
     implementation("androidx.appcompat:appcompat:1.2.0")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.2")
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.1.1")
     implementation("io.ktor:ktor-client-cio:1.4.3")
 
     testImplementation("junit:junit:4.13.1")
+    testImplementation("androidx.test:core:1.3.0")
+    testImplementation("org.mockito:mockito-core:1.10.19")
+    testImplementation("org.json:json:20201115")
     androidTestImplementation("androidx.test.ext:junit:1.1.2")
 }
