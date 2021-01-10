914e4961d09706dd36e1f063d61c66db1de4cc51
atwzj233 <atwzj233@gmail.com>
2021-01-10 22:30:09 +0800

    Add unit tests for OfficialPackageCDNRepository.

---
+++ webapi/src/test/java/org/wvt/horizonmgr/webapi/pack/OfficialPackageCDNRepositoryTest.kt
@@ -0,0 +1,17 @@
+package org.wvt.horizonmgr.webapi.pack
+
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+
+class OfficialPackageCDNRepositoryTest {
+
+    @Test
+    fun getAllPackages() = runBlocking {
+        val pack = OfficialPackageCDNRepository()
+        pack.getAllPackages().forEach {
+            println("uuid: ${it.uuid}, chunks: [${it.chunks.joinToString()}], isSuggested: ${it.isSuggested}, graphicsUrl: ${it.graphicsUrl}")
+            println(it.getManifest())
+            println(it.getChangeLog())
+        }
+    }
+}
