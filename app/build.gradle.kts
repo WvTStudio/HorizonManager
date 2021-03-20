40a955f7a51009b928172d1cdb8a50ff7f6ccdde
atwzj233 <atwzj233@gmail.com>
2021-03-20 15:11:37 +0800

    beta3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "org.wvt.horizonmgr"
         minSdkVersion(21)
         targetSdkVersion(30)
-        versionCode = 15
+        versionCode = 16
         versionName = "2.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -36,7 +36,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 file("proguard-rules.pro")
             )
-            versionNameSuffix = "-beta2"
+            versionNameSuffix = "-beta3"
         }
         create("alpha") {
             proguardFiles = mutableListOf(
