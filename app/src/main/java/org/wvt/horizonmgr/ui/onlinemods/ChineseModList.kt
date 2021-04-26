9de22986231a944dc00a0ea2bbf080a43ac43732
Konyaco <atwzj233@gmail.com>
2021-04-26 12:22:10 +0800

    Code: Add SwipeRefresh to PackageManager.

--- app/src/main/java/org/wvt/horizonmgr/ui/onlinemods/ChineseModList.kt
+++ app/src/main/java/org/wvt/horizonmgr/ui/onlinemods/ChineseModList.kt
@@ -4,9 +4,11 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import org.wvt.horizonmgr.ui.components.EmptyPage
 
 @Composable
 fun ChineseModList(
@@ -19,9 +21,14 @@ fun ChineseModList(
         modifier = modifier,
         contentPadding = PaddingValues(top = 8.dp, bottom = 64.dp)
     ) {
-        itemsIndexed(mods) { index: Int, item: ChineseModModel ->
+        if (mods.isEmpty()) item {
+            EmptyPage(Modifier.fillParentMaxSize()) {
+                Text("什么模组都没有")
+            }
+        } else itemsIndexed(mods) { index: Int, item: ChineseModModel ->
             ChineseModItem(
-                modifier = Modifier.fillParentMaxWidth()
+                modifier = Modifier
+                    .fillParentMaxWidth()
                     .padding(horizontal = 16.dp, vertical = 8.dp),
                 model = item,
                 onClick = { onItemClick(index) },
--- app/src/main/java/org/wvt/horizonmgr/ui/onlinemods/OfficialMirrorModList.kt
+++ app/src/main/java/org/wvt/horizonmgr/ui/onlinemods/OfficialMirrorModList.kt
@@ -4,9 +4,11 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import org.wvt.horizonmgr.ui.components.EmptyPage
 
 @Composable
 fun OfficialMirrorModList(
@@ -19,7 +21,11 @@ fun OfficialMirrorModList(
         modifier = modifier,
         contentPadding = PaddingValues(top = 8.dp, bottom = 64.dp)
     ) {
-        itemsIndexed(mods) { index: Int, item: OfficialMirrorModModel ->
+        if (mods.isEmpty()) item {
+            EmptyPage(Modifier.fillParentMaxSize()) {
+                Text("什么模组都没有")
+            }
+        } else itemsIndexed(mods) { index: Int, item: OfficialMirrorModModel ->
             OfficialMirrorModItem(
                 modifier = Modifier
                     .fillParentMaxWidth()
