3bd40b40a038937ff90353b63a70b342606c3d8b
Konyaco <atwzj233@gmail.com>
2021-08-26 12:12:55 +0800

    Migrate to Coil.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -137,8 +137,8 @@ dependencies {
 
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.2.1")
 
-    implementation("com.github.bumptech.glide:glide:4.12.0")
-    kapt("com.github.bumptech.glide:compiler:4.12.0")
+//    implementation("com.github.bumptech.glide:glide:4.12.0")
+//    kapt("com.github.bumptech.glide:compiler:4.12.0")
 
     val markwonVersion = "4.6.2"
     implementation("io.noties.markwon:core:$markwonVersion")
